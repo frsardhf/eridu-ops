ed34cbf33a6f5bfcb9696810e0d8bb77e3193611
faris ardhafa <frsardhafa@gmail.com>
2025-05-21 13:33:27 +0700

    Fixing postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 import type { PreRenderedAsset } from 'rollup'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer(),
-      ],
-    },
-  },
   base: '/',
   server: {
     headers: {
