dd3355086cf976512d9d83ac78eb6d3d93d52670
faris ardhafa <frsardhafa@gmail.com>
2025-06-04 07:34:58 +0700

    Update contact modal

--- src/components/navbar/ContactModal.vue
+++ src/components/navbar/ContactModal.vue
@@ -23,7 +23,7 @@ function closeModal(event: MouseEvent) {
       
       <div class="modal-content">
         <div class="contact-section">
-          <p>If you have any inquiries or want to report bugs, you can contact me on Discord or X (Twitter):</p>
+          <p>If you have any inquiries or want to report bugs, you can contact me on Discord:</p>
           
           <a href="https://discord.com/users/idxyllic" target="_blank" rel="noopener noreferrer" class="contact-link">
             <div class="contact-item">
@@ -35,21 +35,6 @@ function closeModal(event: MouseEvent) {
               <div class="contact-info">
                 <h4>Discord</h4>
                 <p>idxyllic</p>
-              </div>
-            </div>
-          </a>
-          
-          <a href="https://x.com/idxyllune" target="_blank" rel="noopener noreferrer" class="contact-link">
-            <div class="contact-item">
-              <div class="contact-icon twitter">
-                <!-- X (Twitter) Logo -->
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-                </svg>
-              </div>
-              <div class="contact-info">
-                <h4>X (Twitter)</h4>
-                <p>@idxyllune</p>
               </div>
             </div>
           </a>
@@ -174,11 +159,6 @@ function closeModal(event: MouseEvent) {
 .contact-icon.discord {
   color: #5865F2;
   background-color: rgba(88, 101, 242, 0.1);
-}
-
-.contact-icon.twitter {
-  color: #000000;
-  background-color: rgba(0, 0, 0, 0.1);
 }
 
 .contact-info {
